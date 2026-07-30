a0ceabf5ca6a94b1c212c7576b6aa9024cc84ca2
hola404error <168797813+hola404error@users.noreply.github.com>
2026-07-30 03:03:06 -0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(MyPlayLayer, PlayLayer) {
+    void toggleMirror(bool p0, bool p1, bool p2, bool p3) {
+        return;
+    }
+};
